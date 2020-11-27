e87ee0d31227612e89c0fcfee2b2661804631d04
ksieuk <iebebd3@gmail.com>
2020-11-27 15:53:13 +0300

    add editing

--- main.py
+++ main.py
@@ -6,40 +6,57 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class DBSample(QMainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
-        self.connection = sqlite3.connect("coffee.sqlite")
-        self.pb_run.clicked.connect(self.select_data)
-        # По умолчанию будем выводить все данные из таблицы films
-        self.textEdit.setPlainText("SELECT * FROM coffee")
-        self.select_data()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pb_load.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pb_save.clicked.connect(self.save_results)
+        self.modified = {}
+        self.titles = None
 
-    def select_data(self):
-        # Получим результат запроса,
-        # который ввели в текстовое поле
-        query = self.textEdit.toPlainText()
-        res = self.connection.cursor().execute(query).fetchall()
-        # Заполним размеры таблицы
-        self.tableWidget.setColumnCount(7)
-        self.tableWidget.setRowCount(0)
-        # Заполняем таблицу элементами
-        for i, row in enumerate(res):
-            self.tableWidget.setRowCount(
-                self.tableWidget.rowCount() + 1)
-            for j, elem in enumerate(row):
-                self.tableWidget.setItem(
-                    i, j, QTableWidgetItem(str(elem)))
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        result = cur.execute("SELECT * FROM coffee WHERE id=?",
+                             (item_id := self.spinBox.text(),)).fetchall()
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        # Если запись не нашлась, то не будем ничего делать
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
 
-    def closeEvent(self, event):
-        # При закрытии формы закроем и наше соединение
-        # с базой данных
-        self.connection.close()
+    def item_changed(self, item):
+        # Если значение в ячейке было изменено,
+        # то в словарь записывается пара: название поля, новое значение
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE coffee SET\n"
+            for key in self.modified.keys():
+                que += "{}='{}'\n".format(key, self.modified.get(key))
+            que += "WHERE id = ?"
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+            self.modified.clear()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = DBSample()
+    ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
